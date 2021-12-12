50e799bcbb38f635c34d62ddaa19b9282300dc55
Billy Kirk <kirk.william.c@gmail.com>
2021-12-11 22:00:36 -0600

    Remove dev tests

--- mod.ts
+++ mod.ts
@@ -4,10 +4,10 @@ export class Geocoder {
   constructor(private apiKey: string) {}
 
   /*
-    Usage:
-        const geocoder = new Geocoder(apiKey);
-        const results = await geocoder.forward("95683");
-    */
+      Usage:
+          const geocoder = new Geocoder(apiKey);
+          const results = await geocoder.forward("95683");
+      */
   async forward(query: string) {
     const url = new URL("http://api.positionstack.com/v1/forward");
     url.searchParams.append("access_key", this.apiKey);
@@ -16,8 +16,3 @@ export class Geocoder {
     return results.data;
   }
 }
-
-const apiKey = Deno.env.get("LOCATION_API_KEY") || "";
-const geocoder = new Geocoder(apiKey);
-const results = await geocoder.forward("95683");
-console.log(results);
